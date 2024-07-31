8e329855df6cbc304610e7aa1ae5e7f4de38a666
Ilya Lebedev <unrealcoolstuff@gmail.com>
2024-07-31 17:44:58 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Block } from './Block';
+import './index.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
+      <Block value={0} currency="USD" />
     </div>
   );
 }
