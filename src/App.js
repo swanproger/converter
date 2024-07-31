e2e1106a6a8357ac21213dff3e3d93064fcd616d
Ilya Lebedev <unrealcoolstuff@gmail.com>
2024-07-31 19:41:26 +0300

    first version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Block } from './Block';
 import './index.scss';
 
 function App() {
+  const [fromCurrency, setrFromCurrency] = useState("RUB")
+  const [toCurrency, setrToCurrency] = useState("USD")
+  const [rates, setRates] = useState({})
+
+  useEffect(() => {
+    fetch('https://www.cbr-xml-daily.ru/daily_json.js').then((res) => res.json()).then((json) => {
+      setRates(json.Valute);
+      console.log(json.Valute);
+    }).catch((err) => {
+      console.warn(err);
+      alert("Не удалось получить ")
+    })
+  }, []);
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block value={0} currency={fromCurrency} onChangeCurrency={setrFromCurrency} />
+      <Block value={0} currency={toCurrency} onChangeCurrency={setrToCurrency} />
     </div>
   );
 }
